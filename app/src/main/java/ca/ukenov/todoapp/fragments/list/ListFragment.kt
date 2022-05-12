a13b5f69c462796461ff9314a47a8dbf41f8feb4
ukenov <ykenovalex97@gmail.com>
2022-05-12 22:58:06 +0500

    add animation nav

--- app/src/main/java/ca/ukenov/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/ca/ukenov/todoapp/fragments/list/ListFragment.kt
@@ -2,10 +2,8 @@ package ca.ukenov.todoapp.fragments.list
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import ca.ukenov.todoapp.R
 import ca.ukenov.todoapp.databinding.FragmentListBinding
@@ -21,15 +19,29 @@ class ListFragment : Fragment() {
     ): View? {
         _binding = FragmentListBinding.inflate(inflater, container, false)
         val view = binding.root
+
+
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+
+        binding.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        setHasOptionsMenu(true)
+
+
         return view
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 }
--- app/src/main/java/ca/ukenov/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/ca/ukenov/todoapp/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package ca.ukenov.todoapp.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import ca.ukenov.todoapp.R
 
 class UpdateFragment : Fragment() {
@@ -13,7 +11,12 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
 }
